3345f604c84da1185b03a9df3a3311c90391fba6
Will Cravitz <wcravitz@uchicago.edu>
2023-12-12 01:31:51 -0600

    first draft of player model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,21 @@
 from typing import Optional
-from sqlmodel import SQLModel, Field
+from datetime import datetime
+from sqlmodel import Field, Relationship, SQLModel
+
+class Player(SQLModel, table=True):
+    person_id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    player_slug: str
+    birthdate: Optional[datetime]
+    school: str
+    country: str
+    height: int
+    weight: int
+    jersey: int
+    position: str
+    roster_status: str
+    team_id: int = Field(foreign_key="team.id")
+    draft_year: Optional[int]
+    draft_round: Optional[int]
+    draft_number: Optional[int]
